ff2f564703a033b938a33e9c2ab167c0521f9456
Karega K. Scott <kkscott@outlook.com>
2024-04-01 13:36:39 -0500

    Initial AWS Serverless Pattern module 1

---
+++ README.md
@@ -0,0 +1,9 @@
+# AWS Serverless Pattern: Module [Food Delivery Server (fds)](https://catalog.workshops.aws/serverless-patterns/en-US)
+
+## [Project Road Map (Pitch)](https://catalog.workshops.aws/serverless-patterns/en-US/business-scenario)
+As more people work remotely, restaurants have experienced huge growth for carry-out orders. Customers want dishes from their favorite restaurants, but they are also more aware and concerned about the environmental impact of delivery services.
+
+A startup company received funding to connect restaurants and nearby customers. The new service will exclusively use electric bike riders to pick up and deliver food to hyperlocal customers. The goal is to create a fast, economical, and environmentally friendly delivery service.
+
+> [!NOTE]
+> This is the [self-service](https://catalog.workshops.aws/serverless-patterns/en-US/logistics-self-service) project option
---
+++ setup.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Remove previous version of Golang
+sudo /etc/alternatives/go
+sudo /etc/alternatives/gofmt
+sudo /usr/bin/go
+sudo /usr/bin/gofmt
+sudo /usr/bin/gocode
+
+# Download and checkout a release <tag> 
+git clone https://github.com/golang/go.git ~/.go
+cd ~/.go
+git checkout go1.22.1 
+
+# Build go commands from source
+cd ~/.go/src
+./all.bash
+
+# Initialize enviroment variables
+echo -e 'export $GOROOT=~/.go ' >> ~/.bashrc 
+echo -e 'export $GOPATH=~/go '  >> ~/.bashrc 
+echo -e 'export $PATH="$PATH:~/.go/bin"' >> ~/.bashrc
+
+mkdir ~/install
+
+# Resize EBS in Cloud9 environment 
+cd ~/install
+wget 'https://ws-assets-prod-iad-r-iad-ed304a55c2ca1aee.s3.us-east-1.amazonaws.com/76bc5278-3f38-46e8-b306-f0bfda551f5a/shared/resize.sh' 
+bash resize.sh 20
+
+#
+# Install Python 3.9
+cd ~/install
+wget 'https://www.python.org/ftp/python/3.9.10/Python-3.9.10.tgz'
+echo 'Extracting Python 3.9...'
+tar xvf Python-3.9.10.tgz > /dev/null 2>&1
+cd Python-*/
+./configure --enable-optimizations
+sudo make altinstall
+echo 'Python 3.9 installed!'
+
+# Set Python 3.9 as the default
+alias python="python3.9"
+echo -e 'alias python="python3.9" ' >> ~/.bashrc 
+
+# Upgrade to latest version of AWS SAM 
+cd ~/install
+wget 'https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip'
+unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+sudo ./sam-installation/install --update
+
+# Upgrade to latest AWS CLI version
+cd ~/install
+wget https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip
+unzip awscli-exe-linux-x86_64.zip
+sudo ./aws/install --update
+
+# Upgrade to latest AWS CDK
+cd ~/install
+LATEST_CDK_URL=$(curl -s https://api.github.com/repos/aws/aws-cdk/releases/latest | grep -oP '"zipball_url": "\K[^"]+')
+wget $LATEST_CDK_URL -O cdk-latest.zip
+unzip cdk-latest.zip -d cdk-latest
+npm install -g ./cdk-latest/aws-aws-cdk-*/
+unset LATEST_CDK_URL
+
+# Install Python libraries
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install aws-lambda-powertools
+python3.9 -m pip install boto3 
+python3.9 -m pip install numpy
+
+# Return to the environment directory where we started
+cd ~/environment
+pwd
+echo 'Setup complete!'
