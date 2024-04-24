fc24fdb1162168831c882fe871396a9f764ea7ff
Karega K. Scott <kkscott@outlook.com>
2024-04-24 17:40:03 -0500

    detail description update

--- usersservice/lambda.tf
+++ usersservice/lambda.tf
@@ -10,7 +10,7 @@ data "archive_file" "userfunctions_lambda_zip" {
 resource "aws_lambda_function" "getusers" {
   filename         = data.archive_file.userfunctions_lambda_zip.output_path
   function_name    = "tablescan"
-  description      = "${var.workshop_stack_base_name}.users.table scan"
+  description      = "${var.workshop_stack_base_name}.users"
   role             = aws_iam_role.userfunctions_lambda_role.arn
   handler          = "tablescan.lambda_handler"
   source_code_hash = data.archive_file.userfunctions_lambda_zip.output_base64sha256
