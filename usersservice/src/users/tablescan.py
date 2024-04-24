dab87b2f54e6686959cb1d0722036c5aec0e11a9
Karega K. Scott <kkscott@outlook.com>
2024-04-24 17:13:17 -0500

    fix table name

--- usersservice/src/users/tablescan.py
+++ usersservice/src/users/tablescan.py
@@ -5,7 +5,7 @@ from boto3.dynamodb.table import TableResource
 from boto3.dynamodb.conditions import Attr
 
 def lambda_handler(event, context):
-    table_name = 'fds.app.users'
+    table_name = 'fds.apps.users'
     db = boto3.resource('dynamodb', 'en-us')
 
     table = db.Table(table_name)
