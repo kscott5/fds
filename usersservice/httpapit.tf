8fcbaa5c6fc75d3b5bf0fc126faa432a57359824
Karega K. Scott <kkscott@outlook.com>
2024-04-24 22:27:16 -0500

    Fix 'Error: creating API Gateway v2 Integration: operation error ApiGatewayV2: CreateIntegration, https response error StatusCode: 400,', with target location on aws_apigatewayv2_route resource

--- usersservice/httpapit.tf
+++ usersservice/httpapit.tf
@@ -1,25 +1,27 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/apigatewayv2_api
 resource "aws_apigatewayv2_api" "getusers" {
-  name          = "${var.workshop_stack_base_name}.lambda.tablescan.users"
+  name          = "${var.workshop_stack_base_name}.api.users.tablescan"
   protocol_type = "HTTP"
 }
 
 resource "aws_apigatewayv2_route" "getusers" {
   api_id    = aws_apigatewayv2_api.getusers.id
-  route_key = "GET /getusers"
+  route_key = "GET /users/tablescan"
+  target = "integrations/${aws_apigatewayv2_integration.getusers.id}"
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/apigatewayv2_integration
 resource "aws_apigatewayv2_integration" "getusers" {
   api_id             = aws_apigatewayv2_api.getusers.id
   integration_type   = "AWS_PROXY"
-  integration_method = "GET"
+  integration_method = "POST"
   integration_uri    = aws_lambda_function.getusers.invoke_arn
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/apigatewayv2_deployment
 resource "aws_apigatewayv2_deployment" "getusers" {
   api_id = aws_apigatewayv2_api.getusers.id
+  
 
   triggers = {
     redeployment = sha1(join(",", tolist([
@@ -36,9 +38,11 @@ resource "aws_apigatewayv2_deployment" "getusers" {
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/apigatewayv2_stage
 resource "aws_apigatewayv2_stage" "getusers" {
-  api_id        = "${var.workshop_stack_base_name}.stage.lambda.getusers"
+  api_id        = aws_apigatewayv2_api.getusers.id
   deployment_id = aws_apigatewayv2_deployment.getusers.id
-  name          = "stage_name"
+  name          = "dev"
+  auto_deploy = true
+  
 }
 output "aws_apigatewayv2_api" {
   value = aws_apigatewayv2_api.getusers.api_endpoint
