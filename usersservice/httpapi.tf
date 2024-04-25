ed9c09d5fb18b41e7800364c7a669d757fbff900
Karega K. Scott <kkscott@outlook.com>
2024-04-25 18:58:56 -0500

    Update the lambda function permission with execution arn

--- usersservice/httpapi.tf
+++ usersservice/httpapi.tf
@@ -44,8 +44,8 @@ resource "aws_lambda_permission" "api_getusers" {
   statement_id  = "AllowHttpGetUsers"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.getusers.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = aws_lambda_function.getusers.arn
+  principal     = "apigateway.${var.region}.amazonaws.com"
+  source_arn    = aws_api_gateway_rest_api.getusers.execution_arn
 }
 output "aws_api_gateway_stage" {
   value = aws_api_gateway_stage.getusers.invoke_url
