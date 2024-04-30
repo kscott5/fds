5f865b00c4bf5c8dd015aa41de4a50da9e77e35e
Karega K. Scott <kkscott@outlook.com>
2024-04-30 10:11:31 -0500

    Remember golang uses a .NET style anonymous types. Eliminate golang code bloat with type TypeName struct { event string path string ... }
    use var TypeName interface{}
    Keeping track of code changes regardless of structure.

---
+++ usersservice/src/go/go.mod
@@ -0,0 +1,9 @@
+module main
+
+go 1.22.1
+
+require (
+	github.com/aws/aws-lambda-go v1.47.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+)
---
+++ usersservice/src/go/userservice.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"go.uber.org/zap"
+)
+
+var logger *zap.Logger
+
+type Event interface{}
+
+// curl -s -X POST http://localhost:8080/2015-03-31/functions/function/invocations -d '{"data": [{"hello": "world"}, {"event": "key"}, {"list": [0,1,2,3,4]} ]}' | jq
+//
+//	{
+//	  "data": [
+//	    {
+//	      "hello": "world"
+//	    },
+//	    {
+//	      "event": "key"
+//	    },
+//	    {
+//	      "list": [
+//	        0,
+//	        1,
+//	        2,
+//	        3,
+//	        4
+//	      ]
+//	    }
+//	  ]
+//	}
+type Event2 struct {
+	Data interface{} `json:"data"`
+}
+
+func lambda_handler(c context.Context, e *Event) (*Event, error) {
+	logger.Info(fmt.Sprintln("Context:", c, "Event: ", &e))
+	return e, nil
+}
+
+func main() {
+	var err error
+	logger, err = zap.NewDevelopment()
+	if err != nil {
+		fmt.Printf("error creating zap logger, error:%v", err)
+		return
+	}
+	logger.Info("starting lambda_handler")
+	lambda.Start(lambda_handler)
+}
