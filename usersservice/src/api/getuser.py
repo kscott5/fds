b65da5fd8bbf57a23af533cdaa7c990690db1cfc
Karega K. Scott <kkscott@outlook.com>
2024-04-30 10:04:44 -0500

    Thoughs on monolithic or microservice1 with lambda. Keeping track of code changes.

---
+++ usersservice/src/api/getuser.py
@@ -0,0 +1,43 @@
+import json
+import os
+import uuid
+
+import boto3
+from boto3.dynamodb.table import TableResource
+from boto3.dynamodb.conditions import Attr # allows use of FilterExpression=Attr('UserId').Exists() or others
+
+# Prepare DynamoDB client
+USERS_TABLE = os.getenv('USERS_TABLE', 'fds.apps.users')
+dynamodb = boto3.resource('dynamodb')
+ddbTable = dynamodb.Table(USERS_TABLE)
+
+def lambda_handler(event, context):
+    route_key = f"{event['httpMethod']} {event['resource']}"
+
+    headers = {
+         'Content-Type': 'application/json',
+         'Access-Control-Allow-Orgins': '*',
+    }
+    status_code = 400
+    response_body = {}
+
+    try:    
+        if route_key == 'GET /users{userid}':
+            ddbResults = ddbTable.get_item(
+                Key={'userid': event['pathParameters']['userid']}
+            )
+
+            if 'Item' in ddbResults:
+                response_body = ddbResults['Item']
+            else:
+                response_body = {}
+            status_code = 200
+
+    except Exception as err:
+        response_body = f"Error: {str(err)}"
+
+    return {
+         'headers': headers,
+         'statuscode': status_code,
+         'body': response_body
+    }
