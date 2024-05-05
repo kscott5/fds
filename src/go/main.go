c3f8e34a1274c9a39cded454d4d1dad0181f1eaf
Karega K. Scott <kkscott@outlook.com>
2024-05-05 01:15:42 -0500

    Prepare api gateway request passthrough and dynamodb scan

--- src/go/main.go
+++ src/go/main.go
@@ -4,52 +4,87 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"reflect"
+	"encoding/json"
+	"os"
+	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
 
-	// s"github.com/aws/aws-sdk-go-v2/aws"
-	// s"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"go.uber.org/zap"
 )
 
-// Generic key-value mapper
-type Event map[string]interface{}
+var table_name string = os.Getenv("FDS_APPS_USERS_TABLE")
+
+// AWS API Gateway Passthrough template format
+// curl -s -X POST http://localhost:2026/2015-03-31/functions/function/invocations -d \
+//
+//	'{
+//			"mapper": { \
+//		 		"hello": "world", \
+//		 		"event": "key", \
+//		 		"list": [0,1,2,3,4] \
+//			} \
+//	}' | jq
+type Request struct {
+	Mapper map[string]interface{} `json:"mapper"`
+}
 
 var logger *zap.Logger
 
-var handlers = make(map[string]func(context.Context, *Event) ([]byte, error), 5)
+type LocalCredentials aws.AnonymousCredentials
 
-func getUser(ctx context.Context, in *Event) ([]byte, error) {
+func (local LocalCredentials) Retrieve(ctx context.Context) (aws.Credentials, error) {
+	return aws.Credentials{
+		AccessKeyID:     os.Getenv("AWS_ACCESS_KEY_ID"),
+		SecretAccessKey: os.Getenv("AWS_SECRET_ACCESS_KEY"),
+		Source:          os.Getenv("AWS_REGION"),
+		CanExpire:       true,
+		Expires:         time.Now().Add(time.Second * 240),
+	}, nil // error
+}
+
+var handlers = make(map[string]func(context.Context, *Request) ([]byte, error), 5)
+
+func getUser(ctx context.Context, in *Request) ([]byte, error) {
 	return nil, errors.New("getusers not available")
 }
 
-func getUsers(ctx context.Context, in *Event) ([]byte, error) {
-	return nil, errors.New("getusers not available")
-}
+func getUsers(ctx context.Context, in *Request) ([]byte, error) {
+	cfg := aws.NewConfig()
+	client := dynamodb.NewFromConfig(*cfg, func(options *dynamodb.Options) {
+		options.Region = os.Getenv("AWS_REGION")
+		options.Credentials = aws.NewCredentialsCache(LocalCredentials{})
+	})
 
-// curl -s -X POST http://localhost:2026/2015-03-31/functions/function/invocations -d '{"data": [{"hello": "world"}, {"event": "key"}, {"list": [0,1,2,3,4]} ]}' | jq
-// curl -s -X POST http://localhost:2026/2015-03-31/functions/function/invocations -d '{"data": {"hello": "world", "event": "key", "list": [0,1,2,3,4]} }' | jq
-// curl -s -X POST http://localhost:2026/2015-03-31/functions/function/invocations -d '{"hello": "world", "event": "key", "list": [0,1,2,3,4]} ' | jq
-func lambda_handler(ctx context.Context, in *Event) (*Event, error) {
-	req := *in
-
-	required := map[string]string{"hello": "string:required", "event": "string:required"}
-	//optional := map[string]string{"list": "[]int"}
-
-	mapper := map[string]interface{}{"hello":"", "event":"", "list": make([]int,0)}
-
-	fmt.Println(mapper)
-
-	v := reflect.ValueOf(req)
-	iter := v.MapRange()
-	for iter.Next() {
-		if _, found := required[iter.Key().String()]; !found {
-			return nil, errors.New("request body not correct")
-		}
+	params := dynamodb.ScanInput{
+		TableName: aws.String(table_name),
 	}
-	
-	return in, nil //errors.New("lambda_handler not available\n")
+
+	if output, err := client.Scan(context.Background(), &params); err != nil {
+		logger.Error(fmt.Sprintln(err))
+		return nil, err
+	} else if data, err := json.Marshal(output.Items); err != nil {
+		logger.Error(fmt.Sprintln(err))
+		return nil, err
+	} else {
+		logger.Info("Scan complete")
+		return data, nil
+	}
+}
+
+// curl -s -X POST http://localhost:2026/2015-03-31/functions/function/invocations -d '{"mapper": {"hello": "world", "event": "key", "list": [0,1,2,3,4]} }' | jq
+// Where "TIn" and "TOut" are types compatible with the "encoding/json" standard library.
+// See https://golang.org/pkg/encoding/json/#Unmarshal for how deserialization behaves
+func lambda_handler(ctx context.Context, in *Request) ([]byte, error) {
+	// Generic example
+	// var s string = in.Mapper["event"].(string)
+	// l := in.Mapper["list"].([]interface{})
+	// sum := l[3].(float64) + l[2].(float64)
+	// fmt.Println(in.Mapper["hello"], s, l[3], "+", l[2], "=", sum)
+
+	return getUsers(ctx, in)
 }
 
 func main() {
