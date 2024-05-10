e1617adf5f662aea2f50cec00c0fd302b2189195
Karega K. Scott <kkscott@outlook.com>
2024-05-10 10:17:37 -0500

    env var user table

--- usage.js
+++ usage.js
@@ -6,4 +6,5 @@ console.log("**********************************************\n");
 console.log(`export/set AWS_ACCESS_KEY_ID=${process.env["AWS_ACCESS_KEY_ID"]}`);
 console.log(`export/set AWS_SECRET_ACCESS_KEY=${process.env["AWS_SECRET_ACCESS_KEY"]}`); 
 console.log(`export/set AWS_REGION=${process.env["AWS_REGION"]}\n\n`);
+console.log(`export/set FDS_APPS_USERS_TABLE=${process.env["FDS_APPS_USERS_TABLE"]}`);
 
