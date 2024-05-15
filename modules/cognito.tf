75896f87dcd633fb0f322f839f8bafd8b3c468f1
Karega K. Scott <kkscott@outlook.com>
2024-05-15 11:36:51 -0500

    localhost was its default not repost.aws

--- modules/cognito.tf
+++ modules/cognito.tf
@@ -73,7 +73,7 @@ output "user_pool_admin_group" {
 }
 
 output "cognito_login_url" {
-  value = "https://${aws_cognito_user_pool_client.user_pool_client.id}.auth.${var.region}.amazoncognito.com/login?client_id=${aws_cognito_user_pool_client.user_pool_client.id}&response_type=code&redirect_uri=http://repost.aws"
+  value = "https://${aws_cognito_user_pool_client.user_pool_client.id}.auth.${var.region}.amazoncognito.com/login?client_id=${aws_cognito_user_pool_client.user_pool_client.id}&response_type=code&redirect_uri=http://localhost"
 }
 
 output "cognito_login_auth_command" {
