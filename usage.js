e061b9d3ab18e61d63377964690d90a0f798262a
Karega K. Scott <kkscott@outlook.com>
2024-05-15 11:22:55 -0500

    Simplify message

--- usage.js
+++ usage.js
@@ -2,9 +2,12 @@ const process = require('node:process');
 console.clear();
 
 console.log("Environment Varaible:");
-console.log("**********************************************\n");
-console.log(`export/set AWS_ACCESS_KEY_ID=${process.env["AWS_ACCESS_KEY_ID"]}`);
-console.log(`export/set AWS_SECRET_ACCESS_KEY=${process.env["AWS_SECRET_ACCESS_KEY"]}`); 
-console.log(`export/set AWS_REGION=${process.env["AWS_REGION"]}\n\n`);
+console.log("************************************************************************\n");
+console.log(`export/set AWS_DEFAULT_REGION=${process.env["AWS_DEFAULT_REGION"]}\n\n`);
 console.log(`export/set FDS_APPS_USERS_TABLE=${process.env["FDS_APPS_USERS_TABLE"]}`);
+console.log(`\n`)
 
+console.log("AWS CLI (access key and secret from IAM center\n")
+console.log("https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html")
+console.log("**************************************************************************\n");
+console.log("aws configure")
