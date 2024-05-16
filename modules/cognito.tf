c53ed082ec64c26106741d0ae37ac6ff69f2162b
Karega K. Scott <kkscott@outlook.com>
2024-05-16 11:57:43 -0500

    enable client secret. use with secret_hash

--- modules/cognito.tf
+++ modules/cognito.tf
@@ -33,18 +33,19 @@ resource "aws_cognito_user_pool" "user_pool" {
 resource "aws_cognito_user_pool_client" "user_pool_client" {
   name = "${var.app_prefix}UserPoolClient"
   explicit_auth_flows = [
+    "ALLOW_ADMIN_USER_PASSWORD_AUTH",
     "ALLOW_USER_PASSWORD_AUTH",
     "ALLOW_USER_SRP_AUTH",
     "ALLOW_REFRESH_TOKEN_AUTH"
   ]
-  generate_secret                      = false
+  generate_secret                      = true
   prevent_user_existence_errors        = "ENABLED"
   refresh_token_validity               = 30
   supported_identity_providers         = ["COGNITO"]
   user_pool_id                         = aws_cognito_user_pool.user_pool.id
   allowed_oauth_flows_user_pool_client = true
-  allowed_oauth_flows                  = ["code"]
-  allowed_oauth_scopes                 = ["email", "openid"]
+  allowed_oauth_flows                  = ["code", "implicit"]
+  allowed_oauth_scopes                 = ["email", "openid", "aws.cognito.signin.user.admin"]
   callback_urls                        = ["http://localhost"]
 }
 
@@ -63,8 +64,7 @@ resource "aws_cognito_user_group" "api_administrator_user_pool_group" {
 output "user_pool" {
   value = aws_cognito_user_pool.user_pool.id
 }
-
-output "user_pool_client" {
+output "user_pool_client_id" {
   value = aws_cognito_user_pool_client.user_pool_client.id
 }
 
