4146957ef8e34aec69f19ed56546333f70c8a6a0
Karega K. Scott <kkscott@outlook.com>
2024-05-16 12:49:59 -0500

    BLAME ME. internal shared files

---
+++ src/go/internal/client.go
@@ -0,0 +1,65 @@
+package client
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
+
+var Headers map[string]string = map[string]string{
+		"content-type": "application/json",
+		"access-control-allow-orgin": "*",
+	}
+
+func RequestKey(httpMethod, resource string) (string, error) {
+	if httpMethod != "" && resource != "" {
+		return fmt.Sprintf("%s %s", httpMethod, resource), nil
+	}
+
+	return "", fmt.Errorf("requires http method and resouce path")
+}
+
+// Local Credentials implements CredentialsProvider.Retrieve method
+type LocalCredentials aws.AnonymousCredentials
+
+func (local LocalCredentials) Retrieve(ctx context.Context) (aws.Credentials, error) {
+	return aws.Credentials{
+		AccessKeyID:     os.Getenv("AWS_ACCESS_KEY_ID"),
+		SecretAccessKey: os.Getenv("AWS_SECRET_ACCESS_KEY"),
+		Source:          os.Getenv("AWS_REGION"),
+		CanExpire:       false,
+		Expires:         time.Now().Add(time.Hour * 1),
+		SessionToken:    os.Getenv("AWS_SESSION_TOKEN"),
+	}, nil // error
+}
+
+func NewDynamodb(tableName string) *dynamodb.Client {
+	cfg := aws.NewConfig()
+	return dynamodb.NewFromConfig(*cfg, func(options *dynamodb.Options) {
+		options.Region = os.Getenv("AWS_REGION")
+		options.Credentials = aws.NewCredentialsCache(LocalCredentials{})
+	})
+}
+
+func ParseJSONRequestBody(data string) (*map[string]string, error) {
+	mapper := make(map[string]string)
+	err := json.Unmarshal([]byte(data), &mapper)
+
+	return &mapper, err
+}
+
+func ParametersExists(parameters map[string]string, requires map[string]string) (error) {
+	for k := range requires {
+		if found := parameters[k]; found == "" {
+			return fmt.Errorf("requires request parameters: %s", requires)
+		}
+	}
+	return nil
+}
+
---
+++ src/go/internal/go.mod
@@ -0,0 +1,17 @@
+module github.com/kscott5/fds/internal
+
+go 1.22.1
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.26.2
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.32.2
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.6 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.9.7 // indirect
+	github.com/aws/smithy-go v1.20.2 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
