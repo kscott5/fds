ebbd1198a491d7d1942c54875243bde3f01d83ee
Karega K. Scott <kkscott@outlook.com>
2024-05-20 16:02:24 -0500

    BLAME ME: order status enums and get user from request context

--- src/orders/services/create.go
+++ src/orders/services/create.go
@@ -20,55 +20,98 @@ import (
 	"go.uber.org/zap"
 )
 
+const (
+	OrderIdAttribute = "id"
+	NonContextualUserId = "placeholder"
+)
+
+type OrderStatus uint
+
+func (status OrderStatus) String() string {
+	switch status {
+	case Placed:
+		return "placed"
+	case Acknowledged:
+		return "acknowledged"
+	case Cancelled:
+		return "cancelled"
+	default:
+		return "invalid"
+	}
+}
+
+const (
+	Invalid OrderStatus = iota
+	Placed
+	Acknowledged
+	Cancelled
+)
+
+type Items struct {
+	Description string  `json:"description"`
+	Quanity     int     `json:"quanity"`
+	Amount      float64 `json:"amount"`
+}
+
+type Order struct {
+	RestaurantId string  `json:"restaurantid"`
+	TotalAmount  float64 `json:"totalamount"`
+	Items        []Items `json:"items"`
+	OrderId      string
+	UserId       string
+	Status       string
+	PlacedOn     string
+}
+
+
+func GetUserFromRequestContext(authorizer map[string]interface{}) (string, error) {
+	userId := NonContextualUserId
+
+	// Cognitio user pool authentication and authorization
+	if cmapper := authorizer["claims"]; cmapper != nil {
+		claims := cmapper.(map[string]string)
+		if found := claims["sub"]; found != "" {
+			userId = claims["sub"]
+		}
+
+		return userId, nil
+	}
+
+	return userId, fmt.Errorf("user id not found with request context")
+}
+
 func CreateOrder(ctx context.Context, request *events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	logger, _ := zap.NewDevelopment()
 	logger.Info("lambda function: dynamodb create new order")
 	logger.Debug(fmt.Sprintf("%v", request.Body))
-
 
 	tableName := os.Getenv("FDS_APPS_ORDERS_TABLE")
 	if tableName == "" {
 		tableName = "FDSAppsOrders"
 	}
 
-	// anonymous structure
-	data := struct { 
-		RestaurantId string `json:"restaurantid"`
-		TotalAmount float64 `json:"totalamount"`
-		Items []string `json:"items"`
-		OrderId string
-		UserId string
-		Status string
-		PlacedOn string 
-	}{ OrderId: uuid.New().String(), Status: "PLACED"}
-
 	// extract and validate request body
-	if err :=json.Unmarshal([]byte(request.Body), &data); err != nil {
+	data := Order{}
+	if err := json.Unmarshal([]byte(request.Body), &data); err != nil {
 		return nil, err
 	}
 
 	requires := map[string]string{"restaurantid": "string", "totalamount": "decimal", "items": "map"}
-	if data.RestaurantId == "" || len(data.Items) == 0  || data.TotalAmount <= 0  {
+	if data.RestaurantId == "" || len(data.Items) == 0 || data.TotalAmount <= 0 {
 		return nil, fmt.Errorf("requires: %s", requires)
 	}
 
-	data.UserId = "PLACEHOLDER"
-	// Cognitio user pool authentication and authorization
-	if cmapper := request.RequestContext.Authorizer["claims"]; cmapper != nil {
-		claims := cmapper.(map[string]string)
-		if found := claims["sub"]; found != "" {
-			data.UserId = claims["sub"]
-		}
-	}
-	
 	data.OrderId = uuid.New().String()
-	data.Status = "PLACED"
+	data.Status = Placed.String()
+	data.UserId, _ = GetUserFromRequestContext(request.RequestContext.Authorizer)
+	data.OrderId = uuid.New().String()
+	data.Status = Placed.String()
 	data.PlacedOn = time.Now().UTC().String()
 
 	order := map[string]interface{}{
 		"orderid": data.OrderId,
-		"userid": data.UserId,
-		"data": data,
+		"userid":  data.UserId,
+		"data":    data,
 	}
 
 	if input, err := attributevalue.MarshalMap(order); err != nil {
