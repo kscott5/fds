084c98ec848a529cc0fdf36f77f53d55294eabe6
Karega K. Scott <kkscott@outlook.com>
2024-05-22 15:32:07 -0500

    keep it simple

--- src/localhost/localhost.go
+++ src/localhost/localhost.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -15,52 +14,11 @@ func main() {
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request){
 		logger.Debug("localhost processing oauth request")
 
-		res.WriteHeader(200)
-		res.Header().Add("content-type", "application/json")
-
-		body := make(map[string]interface{},1)
-		body["status"] = 200
-
-		query := req.URL.Query()
-		if query.Has("error") && query.Has("error_description") {
-			body["error"] = query.Get("error")
-			body["error_description"] = query.Get("error_description")
-
-			buffer, _ := json.Marshal(body)
-			res.Write(buffer)
-			return
-		}
-		
-		data := make(map[string]string,1)
-		if query.Has("id_token")  {			
-			data["id_token"] = query.Get("id_token")
-		}
-
-		if query.Has("access_token") {
-			data["access_token"] = query.Get("access_token")
-		}
-
-		if query.Has("token_type") {
-			data["token_type"] = query.Get("token_type")
-		}
-
-		if query.Has("expires_in") {
-			data["expires_in"] = fmt.Sprintf("%s sec", query.Get("expires_in"))
-		}
-
-		if query.Has("state") {
-			data["state"] = query.Get("state")
-		}
-
-		if len(data) == 5 {
-			body["message"] = "invalid response from oauth2/authorize request"
-		} else { 
-			body["message"] = "success"
-			body["data"] = data
-		}
-
-		buffer, _ := json.Marshal(body)
-		res.Write(buffer)
+		res.Write([]byte("<h1>Keep it simple</h1>"))
+		res.Write([]byte("The address bar contains either the access_token and id_token or error message<br/><br/>"))
+		res.Write([]byte("Access API gateway with curl.<br/><br/>"))
+		res.Write([]byte("NOTE: the hash tag is reserve use with web or SPA apps. <br/>"))
+		res.Write([]byte("Example: javascript:document.location.hash"))
 	})
 
 	logger.Info("Starting localhost on port: 8080")
